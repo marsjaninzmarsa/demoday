11d6b7cbec9a2d0b6fba6636dcbbb60b33d8dc5d
Jakub Niewiarowski <jakub@niewiarowski.it>
2020-11-05 11:55:02 +0100

    enqueue global styles.

--- wp-content/themes/packhelp/functions.php
+++ wp-content/themes/packhelp/functions.php
@@ -11,3 +11,17 @@ if (defined('WEBPACK_SERVER')) {
         wp_enqueue_style('main-css');
     });
 }
+
+add_action( 'wp_enqueue_scripts', function() {
+	wp_register_style('foundation', 'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation.min.css');
+	wp_enqueue_style('foundation');
+
+	wp_register_style('motion-ui', 'https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.2.3/motion-ui.min.css');
+	wp_enqueue_style('motion-ui');
+
+	wp_register_style('foundation-prototype', 'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/css/foundation-prototype.min.css', ['foundation']);
+	wp_enqueue_style('foundation-prototype');
+
+	wp_register_style('foundation-icons', 'https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css', ['foundation']);
+	wp_enqueue_style('foundation-icons');
+});
