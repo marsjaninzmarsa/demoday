eb31dae166e81308dfa39ca941edf0f307205e70
Jakub Niewiarowski <jakub@niewiarowski.it>
2020-11-05 16:23:16 +0100

    blog footer (mockuped).

---
+++ wp-content/themes/packhelp/footer-blog.php
@@ -0,0 +1,59 @@
+<footer>
+    <div class="grid-x grid-margin-x expanded callout secondary">
+
+        <div class="large-4 cell hide-for-small-only">
+            <h5>FLICKR IMAGES</h5>
+            <div class="grid-x medium-up-4">
+                <div class="cell"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+                <div class="cell"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+                <div class="cell"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+                <div class="cell"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+                <div class="cell"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+                <div class="cell"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+                <div class="cell"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+                <div class="cell"><img class="thumbnail" src="https://placehold.it/75" alt="image of space dog"></div>
+            </div>
+
+        </div>
+
+        <div class="medium-6 large-4 cell">
+            <h5>FLICKR IMAGES</h5>
+            <span class="secondary label">Space</span>
+            <span class="secondary label">Galaxies</span>
+            <span class="secondary label">Milky Way</span>
+            <span class="secondary label">Black Holes</span>
+            <span class="secondary label">Rebels</span>
+            <span class="secondary label">Death Star</span>
+            <span class="secondary label">Republic</span>
+            <span class="secondary label">R2D2</span>
+        </div>
+
+        <div class="large-4 cell">
+            <h5>RANDOM MAG</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti quam voluptatum vel repellat ab
+                similique molestias molestiae ea omnis eos, id asperiores est praesentium, voluptate officia nulla
+                aspernatur sequi aliquam.</p>
+        </div>
+
+    </div>
+    <div class="grid-x grid-margin-y expanded">
+
+        <div class="small-12 medium-6 cell">
+            <ul class="menu">
+                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+            </ul>
+        </div>
+
+        <div class="small-12 medium-6 cell">
+            <ul class="menu align-right">
+                <li class="menu-text">Copyright © <?= date('Y'); ?> Hello World Company</li>
+            </ul>
+        </div>
+    </div>
+</footer>
+
+        <?php wp_footer(); ?>
+
+    </body>
+</html>
