a59bec91beb159f5565a97064040e0d2a4ab968f
Jakub Niewiarowski <jakub@niewiarowski.it>
2020-11-05 12:06:47 +0100

    enqueue global scripts.

--- wp-content/themes/packhelp/functions.php
+++ wp-content/themes/packhelp/functions.php
@@ -24,4 +24,11 @@ add_action( 'wp_enqueue_scripts', function() {
 
 	wp_register_style('foundation-icons', 'https://cdnjs.cloudflare.com/ajax/libs/foundicons/3.0.0/foundation-icons.css', ['foundation']);
 	wp_enqueue_style('foundation-icons');
+
+	wp_register_script( 'foundation', 'https://cdnjs.cloudflare.com/ajax/libs/foundation/6.4.3/js/foundation.min.js', ['jquery'], '', true );
+	wp_add_inline_script( 'foundation', '$(document).foundation();');
+	wp_enqueue_script('foundation');
+
+	wp_register_script( 'motion-ui', 'https://cdnjs.cloudflare.com/ajax/libs/motion-ui/1.2.3/motion-ui.min.js', ['jquery'], '', true );
+	wp_enqueue_script('motion-ui');
 });
